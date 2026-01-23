e30ab523f2d966af42856ac0190f10dfee1ac137
ANRanga <anranga95@gmail.com>
2026-01-23 12:56:49 -0600

    updated kumu map with sidebar

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -2,7 +2,7 @@ function MapPage() {
   return (
     <div className="map-page">
       <iframe
-        src="https://embed.kumu.io/5c8a5c804f3ad14f33265cfd361fa0d0"
+        src="https://embed.kumu.io/ee7c1f35d212e7379fa87b8a970832ed"
         width="100%"
         height="100%"
         frameBorder="0"
